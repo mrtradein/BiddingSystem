7a451bd096e11b593ac832366b77139d4ee54466
mrtradein <ferry-s3@ulster.ac.uk>
2019-08-16 14:39:12 +0800

    Bidding System made for Rocket Flair Recycling Sim

---
+++ BiddingSystem/Intermediate/Build/Mac/UE4Editor/Inc/BiddingSystem/BiddingSystem.init.gen.cpp
@@ -0,0 +1,35 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeBiddingSystem_init() {}
+	UPackage* Z_Construct_UPackage__Script_BiddingSystem()
+	{
+		static UPackage* ReturnPackage = nullptr;
+		if (!ReturnPackage)
+		{
+			static const UE4CodeGen_Private::FPackageParams PackageParams = {
+				"/Script/BiddingSystem",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x508F40A0,
+				0xE5EA8F87,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
+		}
+		return ReturnPackage;
+	}
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ BiddingSystem/Intermediate/Build/Mac/UE4Editor/Inc/BiddingSystem/BiddingSystemGameModeBase.gen.cpp
@@ -0,0 +1,86 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "BiddingSystem/BiddingSystemGameModeBase.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeBiddingSystemGameModeBase() {}
+// Cross Module References
+	BIDDINGSYSTEM_API UClass* Z_Construct_UClass_ABiddingSystemGameModeBase_NoRegister();
+	BIDDINGSYSTEM_API UClass* Z_Construct_UClass_ABiddingSystemGameModeBase();
+	ENGINE_API UClass* Z_Construct_UClass_AGameModeBase();
+	UPackage* Z_Construct_UPackage__Script_BiddingSystem();
+// End Cross Module References
+	void ABiddingSystemGameModeBase::StaticRegisterNativesABiddingSystemGameModeBase()
+	{
+	}
+	UClass* Z_Construct_UClass_ABiddingSystemGameModeBase_NoRegister()
+	{
+		return ABiddingSystemGameModeBase::StaticClass();
+	}
+	struct Z_Construct_UClass_ABiddingSystemGameModeBase_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_ABiddingSystemGameModeBase_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_AGameModeBase,
+		(UObject* (*)())Z_Construct_UPackage__Script_BiddingSystem,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_ABiddingSystemGameModeBase_Statics::Class_MetaDataParams[] = {
+		{ "HideCategories", "Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation" },
+		{ "IncludePath", "BiddingSystemGameModeBase.h" },
+		{ "ModuleRelativePath", "BiddingSystemGameModeBase.h" },
+		{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_ABiddingSystemGameModeBase_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<ABiddingSystemGameModeBase>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_ABiddingSystemGameModeBase_Statics::ClassParams = {
+		&ABiddingSystemGameModeBase::StaticClass,
+		nullptr,
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x009002A8u,
+		METADATA_PARAMS(Z_Construct_UClass_ABiddingSystemGameModeBase_Statics::Class_MetaDataParams, ARRAY_COUNT(Z_Construct_UClass_ABiddingSystemGameModeBase_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_ABiddingSystemGameModeBase()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_ABiddingSystemGameModeBase_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(ABiddingSystemGameModeBase, 1373516534);
+	template<> BIDDINGSYSTEM_API UClass* StaticClass<ABiddingSystemGameModeBase>()
+	{
+		return ABiddingSystemGameModeBase::StaticClass();
+	}
+	static FCompiledInDefer Z_CompiledInDefer_UClass_ABiddingSystemGameModeBase(Z_Construct_UClass_ABiddingSystemGameModeBase, &ABiddingSystemGameModeBase::StaticClass, TEXT("/Script/BiddingSystem"), TEXT("ABiddingSystemGameModeBase"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(ABiddingSystemGameModeBase);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
