a3871a7e649a4cb84b297167c5be80dfce265947
mrtradein <ferry-s3@ulster.ac.uk>
2019-08-16 14:39:12 +0800

    Bidding System made for WAFA Games Recycling Sim

---
+++ BiddingSystem/Intermediate/Build/Mac/UE4Editor/Inc/BiddingSystem/BiddingSystemGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BIDDINGSYSTEM_BiddingSystemGameModeBase_generated_h
+#error "BiddingSystemGameModeBase.generated.h already included, missing '#pragma once' in BiddingSystemGameModeBase.h"
+#endif
+#define BIDDINGSYSTEM_BiddingSystemGameModeBase_generated_h
+
+#define BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_RPC_WRAPPERS
+#define BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesABiddingSystemGameModeBase(); \
+	friend struct Z_Construct_UClass_ABiddingSystemGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABiddingSystemGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/BiddingSystem"), NO_API) \
+	DECLARE_SERIALIZER(ABiddingSystemGameModeBase)
+
+
+#define BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesABiddingSystemGameModeBase(); \
+	friend struct Z_Construct_UClass_ABiddingSystemGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABiddingSystemGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/BiddingSystem"), NO_API) \
+	DECLARE_SERIALIZER(ABiddingSystemGameModeBase)
+
+
+#define BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABiddingSystemGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABiddingSystemGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABiddingSystemGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABiddingSystemGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABiddingSystemGameModeBase(ABiddingSystemGameModeBase&&); \
+	NO_API ABiddingSystemGameModeBase(const ABiddingSystemGameModeBase&); \
+public:
+
+
+#define BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABiddingSystemGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABiddingSystemGameModeBase(ABiddingSystemGameModeBase&&); \
+	NO_API ABiddingSystemGameModeBase(const ABiddingSystemGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABiddingSystemGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABiddingSystemGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABiddingSystemGameModeBase)
+
+
+#define BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_12_PROLOG
+#define BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_RPC_WRAPPERS \
+	BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_INCLASS \
+	BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> BIDDINGSYSTEM_API UClass* StaticClass<class ABiddingSystemGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID BiddingSystem_Source_BiddingSystem_BiddingSystemGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
